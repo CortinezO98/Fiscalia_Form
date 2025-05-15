afe179b2f43ece64d08669ecbd640dc55623f8d0
CortinezO98 <jcortinezosorio@gmail.com>
2025-05-15 00:34:43 -0500

    Creación Seed Segmentos

--- form_fiscalia/gestion/management/commands/seed_gestion.py
+++ form_fiscalia/gestion/management/commands/seed_gestion.py
@@ -1,8 +1,25 @@
 from django.core.management.base import BaseCommand
+from django.db import transaction
 from gestion.models import *
+from gestion.management.data.segmentos import segmentos
 
 class Command(BaseCommand):
     help = 'Carga datos de prueba en la base de datos gestion'
 
     def handle(self, *args, **kwargs):
-        self.stdout.write(self.style.SUCCESS('Datos de prueba cargados correctamente. gestion'))
+        try:
+            with transaction.atomic():
+                CrearSegmentos()
+                self.stdout.write(self.style.SUCCESS('Datos de prueba cargados correctamente.'))
+        except Exception as e:
+            self.stdout.write(self.style.SUCCESS('Error, se hizo rollback'+e))
+
+
+def CrearSegmentos():
+    for segmentoData in segmentos:
+        segmento = Segmento.objects.filter(id=segmentoData['id']).first()
+        if not segmento:
+            Segmento.objects.create(
+                id=segmentoData['id'],
+                nombre=segmentoData['nombre']
+            )
---
+++ form_fiscalia/gestion/management/data/segmentos.py
@@ -0,0 +1,18 @@
+segmentos = [
+    {"id": 1, "nombre": "Nivel 1"},
+    {"id": 2, "nombre": "Nivel 1 / Creación de denuncias"},
+    {"id": 3, "nombre": "Nivel 2"},
+    {"id": 4, "nombre": "Click to call"},
+    {"id": 5, "nombre": "Videollamada"},
+    {"id": 6, "nombre": "Lengua de señas"},
+    {"id": 7, "nombre": "Psicologo"},
+    {"id": 8, "nombre": "Call Back"},
+    {"id": 9, "nombre": "Chat"},
+    {"id": 10, "nombre": "Chat SMS"},
+    {"id": 11, "nombre": "PQRS (Opción 3)"},
+    {"id": 12, "nombre": "Orientación información y consultas (Opc 4 IVR)"},
+    {"id": 13, "nombre": "Anónimas (Opc 2.2 IVR)"},
+    {"id": 14, "nombre": "Bilingüe Español"},
+    {"id": 15, "nombre": "Bilingüe Ingles"},
+    {"id": 16, "nombre": "Consulta jurídica"},
+]
